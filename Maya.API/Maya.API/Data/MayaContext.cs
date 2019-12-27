233c006ff6f931564512842b47657159f24bc408
Your Name <djshres@gmail.com>
2019-12-27 06:12:38 +0545

    Initial

---
+++ Maya.API/Maya.API/Data/MayaContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Maya.API.Models;
+
+    public class MayaContext : DbContext
+    {
+        public MayaContext (DbContextOptions<MayaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Value> Value { get; set; }
+    }
